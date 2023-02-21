52fc91cf5a2c693499eac9e976c964687535b9dd
TingSyuanWang <vincent@fishboneapps.com>
2023-02-21 14:43:50 -0500

    feat: JSON Decoding
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

---
+++ WeatherManager.swift
@@ -0,0 +1,51 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Ting-Hsuan Wang on 2/21/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=0fe8db36d6408a6376e6ce597069e6bd&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        // 1. Create a URL
+        if let url = URL(string: urlString) {
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
+            
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+    }
+}
